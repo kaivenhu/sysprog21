77ce62b7666afb9714f97ac7531ba3447ee7bbc0
Kai-Wen Hu <kainbd0609@gmail.com>
2020-05-11 21:18:11 +0800

    <xstring> Add unittest for copy-on-write feature

--- xstring/xs-unittest.c
+++ xstring/xs-unittest.c
@@ -50,6 +50,11 @@ void test(void)
     check(xs_data(&string), "((((((((((((foobarbar))))))))))))");
     xs_trim(&string, "()");
     check(xs_data(&string), "foobarbar");
+    xs *copy = xs_copy(&string, &xs_literal_empty());
+    check(xs_data(copy), "foobarbar");
+
+    xs_free(copy);
+    check(xs_data(&string), "foobarbar");
 
     xs_free(&string);
     xs_free(&prefix);
@@ -58,10 +63,50 @@ void test(void)
     xs_free(&empty);
 }
 
+void copy_on_write(void)
+{
+    xs empty = *xs_tmp("");
+    xs *x = xs_new(&xs_literal_empty(), "0123456789abcdef");
+    xs *y = xs_copy(x, &xs_literal_empty());
+    xs *z = xs_copy(x, &xs_literal_empty());
+    xs *w = xs_copy(x, &xs_literal_empty());
+    check(xs_data(x), "0123456789abcdef");
+    check(xs_data(y), "0123456789abcdef");
+    check(xs_data(z), "0123456789abcdef");
+    check(xs_data(w), "0123456789abcdef");
+
+    xs_concat(y, &empty, x);
+    xs_concat(z, x, x);
+    xs_trim(w, "0123");
+    check(xs_data(x), "0123456789abcdef");
+    check(xs_data(y), "0123456789abcdef" "0123456789abcdef");
+    check(xs_data(z), "0123456789abcdef" "0123456789abcdef" "0123456789abcdef");
+    check(xs_data(w), "456789abcdef");
+
+    w = xs_free(w);
+    z = xs_free(z);
+    w = xs_copy(y, &xs_literal_empty());
+    z = xs_copy(y, &xs_literal_empty());
+    check(xs_data(y), "0123456789abcdef" "0123456789abcdef");
+    check(xs_data(w), "0123456789abcdef" "0123456789abcdef");
+    check(xs_data(z), "0123456789abcdef" "0123456789abcdef");
+
+    xs_concat(x, &empty, z);
+    check(xs_data(x), "0123456789abcdef" "0123456789abcdef" "0123456789abcdef");
+    xs_trim(x, "0");
+    check(xs_data(x), "123456789abcdef" "0123456789abcdef" "0123456789abcdef");
+
+    xs_free(x);
+    xs_free(y);
+    xs_free(z);
+    xs_free(w);
+    return;
+}
 
 int main()
 {
     test();
+    copy_on_write();
 
     printf("PASS !!!\n");
     return 0;
