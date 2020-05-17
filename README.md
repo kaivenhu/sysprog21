568094969632bb770ecabaff3df5da842151a464
Kai-Wen Hu <kainbd0609@gmail.com>
2020-05-17 18:07:00 +0800

    <xor-linkedlist> Init XOR linked list project

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Practices for linux program from course [sysprog](http://wiki.csie.ncku.edu.tw/l
 
 ## Projects
 * [XString](https://hackmd.io/@sysprog/linux2020-quiz2) - Small string optimization with copy on write.
+* [XOR linked list](https://hackmd.io/@sysprog/linux2020-quiz3) - Use bitwise XOR operation to implement doubly linked lists.
---
+++ xor-linkedlist/xor-linkedlist.c
@@ -0,0 +1,82 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef struct __list list;
+struct __list {
+    int data;
+    struct __list *addr;
+};
+
+#define XOR(a, b) ((list *) ((uintptr_t)(a) ^ (uintptr_t)(b)))
+
+void insert_node(list **l, int d)
+{
+    list *tmp = malloc(sizeof(list));
+    tmp->data = d;
+
+    if (!(*l)) {
+        tmp->addr = NULL;
+    } else {
+        (*l)->addr = XOR(tmp, (*l)->addr);
+        tmp->addr = *l;
+    }
+    *l = tmp;
+}
+
+void delete_list(list *l)
+{
+    while (l) {
+        list *next = l->addr;
+        if (next)
+            next->addr = XOR(next->addr, l);
+        free(l);
+        l = next;
+    }
+}
+
+list *sort(list *start)
+{
+    if (!start || !start->addr)
+        return start;
+
+    list *left = start, *right = start->addr;
+    left->addr = NULL;
+    right->addr = XOR(right->addr, left);
+
+    left = sort(left);
+    right = sort(right);
+
+    for (list *merge = NULL; left || right;) {
+        if (!right || (left && left->data < right->data)) {
+            list *next = left->addr;
+            if (next)
+                next->addr = XOR(left, next->addr);
+
+            if (!merge) {
+                start = merge = left;
+                merge->addr = NULL;
+            } else {
+                merge->addr = LL1;
+                left->addr = LL2;
+                merge = left;
+            }
+            left = next;
+        } else {
+            list *next = right->addr;
+            if (next)
+                next->addr = XOR(right, next->addr);
+
+            if (!merge) {
+                start = merge = right;
+                merge->addr = NULL;
+            } else {
+                merge->addr = RR1;
+                right->addr = RR2;
+                merge = right;
+            }
+            right = next;
+        }
+    }
+
+    return start;
+}
