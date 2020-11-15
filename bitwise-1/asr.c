2b39e7c2d19e44318b203f892fd96ade9c3aaa73
Kai-Wen Hu <kainbd0609@gmail.com>
2020-11-15 17:16:36 +0800

    <bitwise> Complete arithmetic right shift function

--- bitwise-1/asr.c
+++ bitwise-1/asr.c
@@ -1,7 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define asr_checker(m, n, ans)                     \
+    do {                                           \
+        int ret = 0;                               \
+        if (ans != (ret = asr_i(m, n))) {          \
+            printf(                                \
+                "Failed with ans: %d, ret: %d on " \
+                "M: %d, N: %u, line %d\n",         \
+                ans, ret, m, n, __LINE__);         \
+            exit(1);                               \
+        }                                          \
+    } while (0)
+
 int asr_i(signed int m, unsigned int n)
 {
-    const int logical = (((int) -1) OP1) > 0;
-    unsigned int fixu = -(logical & (OP2));
+    const int logical = (((int) -1) >> 1) > 0;
+    unsigned int fixu = -(logical & (m < 0));
     int fix = *(int *) &fixu;
     return (m >> n) | (fix ^ (fix >> n));
+}
+
+int main(void)
+{
+    asr_checker(-5, 1, -3);
+    asr_checker(5, 2, 5 >> 2);
+    asr_checker(0xFFFFF, 0xF, 0xFFFFF >> 0xF);
+    asr_checker(-20, 3, -3);
+    printf("PASS !!!\n");
+    return 0;
 }
